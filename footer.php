027a48f4a65e0bf8212b1a24f2a0bf26e9b442e3
pedrogab96 <39196117+pedrogab96@users.noreply.github.com>
2018-07-14 23:57:52 -0300

    Footer
    Adicionando o footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
-    <footer>
-        
+<footer class="footer">
+        <section class="footer-text">
+            <p>&copy Sistema de Apostas - 2018. Todos os direitos reservados</p>
+        </section>
     </footer>
-    
     </body>
 </html>
