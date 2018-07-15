3d0c3d53d0608776be02259689ea91cd26fbf666
Edilson Fernandes <31668778+Humble23@users.noreply.github.com>
2018-07-15 00:58:16 -0300

    Merge branch 'master' of https://github.com/ZonaNegra/SistemasdeApostas

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php include("header.php");?>
-    <h1>Hello World</h1>
-    <h1>Pedroaorksa rhajeahjehhjsaehjejah</h1>
     <section class ='my-slide'>
         <h1>Aqui vai ser um slide</h1>
     </section>
