82ce244d5f8cfea2d82860329884b0a8212488a4
pedrogab96 <39196117+pedrogab96@users.noreply.github.com>
2018-07-15 14:08:39 -0300

    teste

---
+++ footer.php
@@ -0,0 +1,8 @@
+
+<footer class="footer">
+        <section class="footer-body">
+            <p class="footer-text-left">&copy Sistema de Apostas - 2018. Todos os direitos reservados</p>
+        </section>
+    </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php include("header.php");?>
+    <h1>Hello World</h1>
+    <section class ='my-slide'>
+        <h1>Aqui vai ser um slide</h1>
+    </section>
+    <section class='content'>
+        <h1>Conteúdo</h1>
+    </section>
+<?php include("footer.php");?>
