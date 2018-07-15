9cc1a78f3f4df063730147cc6ae7ab421a9117b8
pedrogab96 <39196117+pedrogab96@users.noreply.github.com>
2018-07-15 00:43:46 -0300

    Upload backup

---
+++ footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+        <section class="footer-text">
+            <p>&copy Sistema de Apostas - 2018. Todos os direitos reservados</p>
+        </section>
+    </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include("header.php");?>
+    <section class ='my-slide'>
+        <h1>Aqui vai ser um slide</h1>
+    </section>
+    <section class='content'>
+        <h1>Conteúdo</h1>
+    </section>
+<?php include("footer.php");?>
