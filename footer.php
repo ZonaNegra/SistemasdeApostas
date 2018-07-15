037a4a6579e54315fb733d3f5a70c4b6acffbf78
pedrogab96 <39196117+pedrogab96@users.noreply.github.com>
2018-07-15 14:12:56 -0300

    testando

--- footer.php
+++
@@ -1,8 +0,0 @@
-
-<footer class="footer">
-        <section class="footer-body">
-            <p class="footer-text-left">&copy Sistema de Apostas - 2018. Todos os direitos reservados</p>
-        </section>
-    </footer>
-    </body>
-</html>
