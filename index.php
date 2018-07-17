424aa2de86612e70e13c19bd7a55e987ddaba16f
mirasatansoul <41263237+mirasatansoul@users.noreply.github.com>
2018-07-16 23:19:41 -0300

    Testando upload de alterações

--- index.php
+++ index.php
@@ -11,9 +11,5 @@
             }
 
         ?>
-        <h1>Aqui vai ser um slide</h1>
-    </section>
-    <section class='content'>
-        <h1>Conteúdo</h1>
     </section>
 <?php include("footer.php");?>
--- pages/contato.php
+++ pages/contato.php
@@ -1,1 +1,13 @@
-<h1>Contato</h1>
+<div class="container" style="background-color: #bbccff">
+    <section id="contato" class="row">
+        <div class="col">
+            Tá
+        </div>
+        <div class="col">
+            ná
+        </div>
+        <div class="col">
+            disney
+        </div>
+    </section>
+</div>
